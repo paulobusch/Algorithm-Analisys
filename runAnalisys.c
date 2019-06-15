31a1ce52e49157d014d60aede22e3938798e50bf
Paulo Ricardo Busch <paulo.ricardo@forlogic.net>
2019-06-15 12:30:42 -0300

    Análise de tempo de execução de algoritmo

--- runAnalisys.c
+++ runAnalisys.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <string.h>
+#include <time.h>
 
 //====== CONSTANTES ========
 #define FOLDER_DATA "data"
@@ -12,19 +14,28 @@
 
 //======= PROTÓTIPOS ========
 char **filesByFolder(char *path, int *qtdFiles);
-char **infoByFile(char *path, int *qtdRows);
+int  *arrayByFile(char *path, int *qtdArray);
+
+void startAnalisys(char *file, char *header);
+void stopAnalisys(char *file, char *header);
+
+char *combinePath(char *path, char *file);
 
 //======= VARIÁVEIS =========
-
+clock_t start, end;
+double cpu_time_used;
 
 //======= MAIN ==========
 int main(){
-    int qtdFiles = 0; 
-    int idxFile = 0;
+    int qtdFiles = 0, idxFile = 0;
+    int qtdArray = 0, idxLine = 0;
     char **files = filesByFolder(FOLDER_DATA, &qtdFiles);
     
     while(idxFile < qtdFiles){
-        puts(files[idxFile]);
+        startAnalisys(files[idxFile], "Iniciado Leitura");
+        arrayByFile(combinePath(FOLDER_DATA,files[idxFile]), &qtdArray);
+        stopAnalisys(files[idxFile], "Parado Leitura");
+        printf(" Valores: %d", qtdArray);
         idxFile++;
     }
 
@@ -36,16 +47,21 @@ char **filesByFolder(char *path, int *qtdFiles){
     DIR *dir = NULL;
     struct dirent *inf = NULL;
     char **files = (char**) malloc(sizeof(char*));
-    int coutFiles = 0;
+    char *file = (char*) malloc(sizeof(char) * FILE_ROW);
     dir = opendir(path);
 
-    if(dir == NULL){      
+    if(dir == NULL)
         return NULL;
-    }
-    
-    for(*qtdFiles = 0; (inf = readdir(dir)) != NULL; *qtdFiles++){
-        files = (char**) realloc(files, (*qtdFiles +1) * sizeof(char**));
-        files[*qtdFiles] = inf->d_name;
+
+    *qtdFiles = 0;
+    while((inf = readdir(dir)) != NULL){
+        strcpy(file,inf->d_name);
+        if(strcmp(file, ".")!=0 && strcmp(file, "..")!=0){
+            files = (char**) realloc(files, (*qtdFiles +1) * sizeof(char**));
+            files[*qtdFiles] = (char*) malloc(sizeof(char) * FILE_ROW);
+            strcpy(files[*qtdFiles],file);
+            (*qtdFiles)++;
+        }
     }
 
     closedir(dir);
@@ -54,19 +70,37 @@ char **filesByFolder(char *path, int *qtdFiles){
 }
 
 //========== ARQUIVOS =========== 
-char **infoByFile(char *path, int *qtdRows){
+int *arrayByFile(char *path, int *qtdArray){
+    int *array = NULL;
     FILE *file = fopen(path, "r");
-    char **rows = (char**) malloc(sizeof(char*));
-    int contRows = 0;
 
-    if(file == NULL){
-
+    if(file == NULL)
         return NULL;
+
+    *qtdArray = 0;
+    while(!feof(file)){
+        (*qtdArray)++;
+        array = (int*) realloc(array, (*qtdArray) * sizeof(int));
+        fscanf(file, "%d", &array[*qtdArray -1]);
     }
 
-    while(fgets(rows[contRows], FILE_ROW, file) != NULL)
-        puts(rows[contRows]);
-
     fclose(file);
-    return rows;
+    return array;
+}
+
+char *combinePath(char *path, char *file){
+    char *pathFile = (char*) malloc(sizeof(char) * FILE_ROW);
+    sprintf(pathFile,"%s/%s",path,file);
+    return pathFile;
+}
+
+//======= GERAÇÃO DE LOG ======== 
+void startAnalisys(char *file, char *header){
+    start = clock();
+}
+
+void stopAnalisys(char *file, char *header){
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("\nArquivo: %s Tempo: %.2fs", file, cpu_time_used);
 }
